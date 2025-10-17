391c5df8c20d7acdd4c32d0cf3a9c377a4f80ee1
Saim-Warsi <kamransaim385@gmail.com>
2025-10-17 17:02:00 +0500

    Added the live backend URL

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user order from frontend
 const placeOrder = async (req,res)=>{
 
-    const frontendUrl = "https://little-lemon-live.netlify.app/"
+    const frontendUrl = "https://little-lemon-live.netlify.app"
     try{
         const newOrder = new orderModel({
             userId:req.body.userId,
