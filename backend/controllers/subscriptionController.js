169a5dd99831999c5206ad9e3d2eed19a5dfe72b
Saim-Warsi <kamransaim385@gmail.com>
2025-10-18 03:30:09 +0500

    controller updated

--- backend/controllers/subscriptionController.js
+++ backend/controllers/subscriptionController.js
@@ -139,8 +139,8 @@ const sendPromotion = async (req, res) => {
         // Configure nodemailer transporter (Using your App Password env vars)
         const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
-            port: 587,
-            secure: false,
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASSWORD, // This should be the Gmail App Password
